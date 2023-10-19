06ccfea1622bfdc74dcf97662c4689b4be927e58
DarmaW9 <147920792+DarmaW9@users.noreply.github.com>
2023-10-20 05:09:50 +0700

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
-import * as React from "react"
+import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const IndexPage = () => (
+  <div>
+    <h1>Curriculum Vitae</h1>
+    <h2>Your Name</h2>
+    <p>Contact: your-email@example.com</p>
+    <h3>Education</h3>
+    <p>Bachelor of Science in Computer Science, University XYZ, 2015-2019</p>
+    // ... add more sections like Experience, Skills, Projects, etc.
+  </div>
+)
+
+export default IndexPage
